6ad3958d5a12a3e3c7f88648e0abadf523060797
Sri Sagiraju <srisagiraju@Sris-MacBook-Pro.local>
2026-01-02 06:56:02 -0600

    debug4 hero section fetch

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import BottomCards from './components/bottomCards';
-import TillyTherapy from './components/TillyTherapy';
-import Services from './components/servicesSection.js';
-import AboutUs from './components/aboutUs'
-import Main from "./components/heroSections"
-import Providers from "./components/providers"
+import React from "react";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import BottomCards from "./components/bottomCards";
+import TillyTherapy from "./components/TillyTherapy";
+import Services from "./components/servicesSection";
+import AboutUs from "./components/aboutUs";
+import Main from "./components/heroSections";
+import Providers from "./components/providers";
 
 function App() {
+  // quick proof the frontend bundle is updated
+  console.warn("🔥 App.js loaded");
+
   return (
     <>
       <Navbar />
-      <Main/>
+      <Main />
       <Services />
-      <AboutUs/>
-      <Providers/>
+      <AboutUs />
+      <Providers />
       <BottomCards />
       <TillyTherapy />
       <Footer />
